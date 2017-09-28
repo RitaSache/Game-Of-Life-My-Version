5f3be3d5f1f30fb6ad259f59e26917b5bf91b8e2
Rita S <margarita7017@gmail.com>
2017-09-27 21:27:36 -0700

    add enter to quit program

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
 		cout << "Provide a mode you want: if Classic mode press (1) and enter, if Doughnut mode press (2) and enter, if Mirror mode press (3) and enter" << endl;
 		cin >> mode;
 		GameOfLife p (mode);
-		cout << "If you want to press Enter type (1), if you want a pause between generations type (2), if you want to output into a file press (3)" << endl; 
+		cout << "If you want to press Enter type (1), if you want a pause between generations type (2), if you want to output into a file press (3), if none of these press (4) " << endl; 
 		cin >> outputOption;
 		
 		if(outputOption == 3) {
@@ -34,6 +34,9 @@ int main() {
 			p.simulateGeneration();
 			p.writeGenerationIntoFile(otherFile);
 			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
 		}
 		else if(outputOption == 1) {
 			p.createPlanetFromFile(fileName);
@@ -45,8 +48,11 @@ int main() {
 				p.printGeneration();
 				cin.get();
 			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
 		}
-		else {
+		else if(outputOption == 2) {
 			p.createPlanetFromFile(fileName);
 			p.printGeneration();
 			system("pause");
@@ -55,6 +61,23 @@ int main() {
 				p.printGeneration();
 				system("pause");
 			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
+		}
+		else if (outputOption == 4) {
+			p.createPlanetFromFile(fileName);
+			p.printGeneration();
+			while(!p.isGenerationEmpty() && !p.isGenerationStable()) { //these should return false the first time 
+				p.simulateGeneration();
+				p.printGeneration();
+			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
+		}
+		else {
+			cout << "Invalid input" << endl;
 		}
 	}
 	else if(mapFile == "no") {
@@ -63,17 +86,68 @@ int main() {
 		cin >> height;
 		cout << "World width: " << endl;
 		cin >> width;
-		cout << "World density: " << endl;
+		cout << "World density (between 0 and 1): " << endl;
 		cin >> density;
 		cout << "Provide a mode you want: if Classic mode press (1) and enter, if Doughnut mode press (2) and enter, if Mirror mode press (3) and enter" << endl;
 		cin >> mode;
 		GameOfLife p (mode);
-		p.createRandomPlanet(height, width, density);
-		p.printGeneration();
-		while(!p.isGenerationEmpty() && !p.isGenerationStable()) { //these should return false the first time 
-			p.simulateGeneration();
-			p.printGeneration();
+
+		cout << "If you want to press Enter type (1), if you want a pause between generations type (2), if you want to output into a file press (3), if none of these press (4) " << endl; 
+		cin >> outputOption;
 		
+		if(outputOption == 3) {
+			cout << "please enter file name to output to" << endl;
+			cin >> otherFile;
+			p.createRandomPlanet(height, width, density);
+			p.writeGenerationIntoFile(otherFile);
+			while(!p.isGenerationEmpty() && !p.isGenerationStable()) { //these should return false the first time 
+			p.simulateGeneration();
+			p.writeGenerationIntoFile(otherFile);
+			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
+		}
+		else if(outputOption == 1) {
+			p.createRandomPlanet(height, width, density);
+			p.printGeneration();
+			cin.get();
+			cin.get();
+			while(!p.isGenerationEmpty() && !p.isGenerationStable()) { //these should return false the first time 
+				p.simulateGeneration();
+				p.printGeneration();
+				cin.get();
+			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
+		}
+		else if(outputOption == 2){
+			p.createRandomPlanet(height, width, density);
+			p.printGeneration();
+			system("pause");
+			while(!p.isGenerationEmpty() && !p.isGenerationStable()) { //these should return false the first time 
+				p.simulateGeneration();
+				p.printGeneration();
+				system("pause");
+			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
+		}
+		else if (outputOption == 4) {
+			p.createRandomPlanet(height, width, density);
+			p.printGeneration();
+			while(!p.isGenerationEmpty() && !p.isGenerationStable()) { //these should return false the first time 
+				p.simulateGeneration();
+				p.printGeneration();
+			}
+			cout << "Press enter to exit the program " << endl;
+			cin.ignore();
+			cin.ignore();
+		}
+		else {
+			cout << "Invalid input" << endl;
 		}
 	}
 
